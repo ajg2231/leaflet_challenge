897511e8985c45196a997432a86d8e2afd0294a6
ajg2231 <43033837+ajg2231@users.noreply.github.com>
2021-04-27 09:33:41 -0400

    Update MEGAMAP.js

--- map1/static/js/MEGAMAP.js
+++ map1/static/js/MEGAMAP.js
@@ -63,7 +63,7 @@ function cram(qua) {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
-    accessToken: KEY
+    accessToken: API_KEY
   });
 
   var bam = {
