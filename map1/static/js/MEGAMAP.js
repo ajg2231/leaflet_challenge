1308e53bf0e453eea12267bef4b4c951675680e7
ajg2231 <43033837+ajg2231@users.noreply.github.com>
2021-04-27 09:06:32 -0400

    Update MEGAMAP.js

--- map1/static/js/MEGAMAP.js
+++ map1/static/js/MEGAMAP.js
@@ -1,108 +1,126 @@
-const KEY = "pk.eyJ1IjoiYWpnMjIzMSIsImEiOiJja252b2Vha20wbDkxMndzNzcwMnhzOTgzIn0.SzxKIeSYOwDNqL_9TNNVsA";
-
-var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-03-16&endtime=" +
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-03-16&endtime=" +
   "2021-04-16&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-d3.json(url, function(data) {
-  make(data.features);
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
   console.log(data);
 });
 
-function oco(depth) {
-
-    if (depth >= 90) {
-      return "blue";
-    }
-    else if (depth >= 70) {
-      return "red";
-    }
-    else if (depth>= 50) {
-     return "green";
-    }
-    else if (depth >= 30) {
-      return "yellow";
-    }
-    else if (depth >= 10) {
-      return "purple";
-    }
-    else {
-      return "orange";
-    }
-
-  }
-
-  function pir(mag) {
-      return mag ** 2;
-    }
-
-function make(eqs) {
-
-    function ola(feature) {
+function createFeatures(earthquakeData) {
+    
+    function onEachLayer(feature) {
         return new L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-          radius: pir(feature.properties.mag),
+          radius: getRadius(feature.properties.mag),
           fillOpacity: 0.8,
-          color: oco(feature.geometry.coordinates[2]),
-          fillColor: oco(feature.geometry.coordinates[2])
+          color: getColor(feature.geometry.coordinates[2]),
+          fillColor: getColor(feature.geometry.coordinates[2])
         });
     }
 
-    function ofe(feature, layer) {
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>Location: " + feature.properties.place +
         "</h3><hr><p><b>Date: </b>" + new Date(feature.properties.time) + "</p><p><b>Magnitude: </b>" + feature.properties.mag + "</p>" + "</p><p><b>Depth: </b>" + feature.geometry.coordinates[2] + "</p>");
     }
 
-  var qua = L.geoJSON(eqs, {
-    onEachFeature: ofe,
-    pointToLayer: ola
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: onEachLayer
   });
 
-  cram(qua);
+
+  createMap(earthquakes);
 }
 
-function cram(qua) {
+function createMap(earthquakes) {
 
-  var dram = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // Define streetmap and darkmap layers
+//   var satmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox.satellite",
+//     accessToken: API_KEY
+//   });
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
-    accessToken: KEY
+    accessToken: API_KEY
   });
 
-  var bam = {
 
-    "Dark Map": dram
+  var baseMaps = {
+    // "Satellite Map": satmap,
+    "Dark Map": darkmap
   };
 
-  var ovl = {
-    Earthquakes: qua
+  var overlayMaps = {
+    Earthquakes: earthquakes
   };
 
-  var ha = L.map("map", {
+  var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [dram, qua]
+    layers: [darkmap, earthquakes]
   });
 
-  L.control.layers(bam, ovl, {
+  L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(ha);
+  }).addTo(myMap);
 
 
-  var leg = L.control({
+
+
+  var legend = L.control({
     position: 'bottomright'
 });
 
-leg.onAdd = function (color) {
+
+legend.onAdd = function (color) {
     var div = L.DomUtil.create('div', 'info legend');
-    dep = ["-10-10", "10-30", "30-50", "50-70", "70-90", "90+"];
-    col = ["blue", "red", "green", "yellow", "purple", "orange"]
+    depth = ["-10-10", "10-30", "30-50", "50-70", "70-90", "90+"];
+    colors = ["#A3F600", "#DCF400", "#F7DB11", "#FDB72A", "#FCA35D", "#FF5F65"]
     div.innerHTML += "<h3>Depth</h3>"
     for (var i = 0; i < depth.length; i++) {
-        div.innerHTML += '<i style="background:' + col[i] + '"></i>' + dep[i] + '<br>';
+        div.innerHTML += '<i style="background:' + colors[i] + '"></i>' + depth[i] + '<br>';
     }
     return div;
 }
-leg.addTo(ha);
+legend.addTo(myMap);
 }
+
+function getColor(depth) {
+    
+    if (depth >= 90) {
+      return "#FF5F65";
+    }
+    else if (depth >= 70) {
+      return "#FCA35D";
+    }
+    else if (depth>= 50) {
+     return "#FDB72A";
+    }
+    else if (depth >= 30) {
+      return "#F7DB11";
+    }
+    else if (depth >= 10) {
+      return "#DCF400";
+    }
+    else {
+      return "#A3F600";
+    }
+  
+  }
+function getRadius(magnitude) {
+    return magnitude ** 2;
+  }
